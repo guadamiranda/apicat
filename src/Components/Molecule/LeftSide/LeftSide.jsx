21ef1beaddf62753c63b155138ad81fcbba7c8e8
Guada <guadamiranda@justiciacordoba.gob.ar>
2023-07-14 10:24:27 -0300

    LeftSide

--- src/Components/Molecule/LeftSide/LeftSide.jsx
+++ src/Components/Molecule/LeftSide/LeftSide.jsx
@@ -1,6 +1,7 @@
 import Button from '@/Components/Atom/Button/Button'
 import style from './leftSide.module.scss'
 import {PiTelevisionSimpleFill} from 'react-icons/pi'
+import { AiFillCloud } from 'react-icons/ai'
 
 const LeftSide = ({setMichiPhotoFunction}) => {
     return(
@@ -9,12 +10,27 @@ const LeftSide = ({setMichiPhotoFunction}) => {
                 <PiTelevisionSimpleFill/><span>MichiVision</span>
             </div>
             <div className={style.leftSideComponent__body}>
-                <div className={style.leftSideComponent__text}>
-                    <div><span className={style.leftSideComponent__italic}>Explora</span> una gran cantidad de imágenes de <span className={style.leftSideComponent__italic}>michis._</span></div>
+                <div className={style.leftSideComponent__textAndButton}>
+                    <div className={style.leftSideComponent__text}>
+                        <div><span className={style.leftSideComponent__italic}>Explora</span> una gran cantidad de imágenes de <span className={style.leftSideComponent__italic}>michis._</span></div>
+                    </div>
+                    <div>
+                        <Button onClickFunction={setMichiPhotoFunction}/>
+                    </div>
                 </div>
-                <div>
-                    <Button onClickFunction={setMichiPhotoFunction}/>
+
+                <div className={style.leftSideComponent__details}>
+                    <div className={style.leftSideComponent__squarePink}>
+                        <AiFillCloud/>
+                    </div>
+                    <div className={style.leftSideComponent__squareOrange}>
+                        <AiFillCloud/>
+                    </div>
+                    <div className={style.leftSideComponent__squarePink}>
+                        <AiFillCloud/>
+                    </div>
                 </div>
+                
                 
             </div>
             
